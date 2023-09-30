a810e1b900eaaf104e0298efcc6023d029261d3e
MC-Stephan <stephan@mehr-conversions.de>
2023-09-30 16:23:03 +0200

    Initial Commit

---
+++ Test.js
@@ -0,0 +1,4 @@
+$('.test').text('moin4');
+document.getElementById("reisebeginn").type = "date";
+document.getElementById("field").type = "date";
+document.getElementById("disablebtn").style.display = "none";
